a96e28067b877c67808ea7b7496b8215f9c4337e
Aaron Mizrachi <aaron@unmanarc.com>
2022-08-03 16:25:23 +0300

    Remove subproject version

--- hellomantids/src/config.h
+++ hellomantids/src/config.h
@@ -2,10 +2,10 @@
 #define PROJECT_VERSION_H
 
 #define PROJECT_NAME "hellomantids"
-#define PROJECT_VER  "1.0.0"
+#define PROJECT_VER  "1.1.1"
 #define PROJECT_DESCRIPTION  "Hello World for libMantids WEB Application - MariaDB version"
 #define PROJECT_VER_MAJOR "1"
-#define PROJECT_VER_MINOR "0"
-#define PROJECT_VER_PATCH "0"
+#define PROJECT_VER_MINOR "1"
+#define PROJECT_VER_PATCH "1"
 
 #endif // PROJECT_VERSION_H
